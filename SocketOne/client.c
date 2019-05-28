eb936397a0aba21461bedd481d155e84de62d513
takamabu <minhthang3321@gmail.com>
2019-05-28 17:37:01 +0700

    fixed code client

--- SocketOne/client.c
+++ SocketOne/client.c
@@ -8,43 +8,45 @@
 
 int main()
 {
-    int sock = 0, valread;
+    int sock = 0;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buffer[1024] = {0};
+    char *StrInput;
     char add[225]="192.168.81.11";
-    int continu = 1;
+    int Continue = 1;
     memset(&serv_addr, '0', sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
+        close(sock);
         return -1;
     }
     if(inet_pton(AF_INET, add, &serv_addr.sin_addr) <= 0)
     {
         printf("\nInvalid address/ Address not supported \n");
+        close(sock);
         return -1;
     }
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         printf("\nConnection Failed \n");
+        close(sock);
         return -1;
     }
-    while(continu == 1){
-        char mess_from_client[225];
-        printf("Nhap noi dung tin nhan gui den server\n");
-        gets(mess_from_client);
+    while(Continue == 1){
+        char StrTemp[225];
+        printf("Please input messenger to sent server\n");
+        gets(StrTemp);
         fflush(stdin);
-        hello = &mess_from_client;
-        send(sock , hello , strlen(hello) , 0 );
-
-        if (strcmp(mess_from_client, "bye") == 0)
+        StrInput = &StrTemp;
+        send(sock , StrInput , strlen(StrInput) , 0 );
+        if (strcmp(StrTemp, "bye") == 0)
         {
-            continu = 0;
+            Continue = 0;
         }
         fflush(stdin);
+
     }
     close(sock);
     return 0;
